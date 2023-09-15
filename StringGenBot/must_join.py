2968c9c127cead5252e0465b8fddc06f147adf93
olinssar <143740876+olinssar@users.noreply.github.com>
2023-09-15 18:17:57 +0300

    Update must_join.py

--- StringGenBot/must_join.py
+++ StringGenBot/must_join.py
@@ -25,7 +25,7 @@ async def must_join_channel(bot: Client, msg: Message):
                         [
                             [
                                 InlineKeyboardButton("</> ᴛᴇᴀᴍ sᴜᴋᴜɴ", url=link),
-                                InlineKeyboardButton("</> sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ", url="https://t.me/NNINB"),
+                                InlineKeyboardButton("</> sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ", url="https://t.me/NKINB"),
                             ]
                         ]
                     )
